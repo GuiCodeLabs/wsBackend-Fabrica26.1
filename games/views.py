d1b879af18aefdf0389a4391d53b7d615b9ed304
Guilherme <guilhermebeserra2007@gmail.com>
2026-03-21 10:33:47 -0300

    feat: associate games with users and restrict access per user

--- games/views.py
+++ games/views.py
@@ -5,18 +5,20 @@ from .forms import GameForm
 
 @login_required
 def game_list(request):
-    games = Game.objects.all()
+    games = Game.objects.filter(user=request.user)
     return render(request, 'games/game_list.html', {'games': games})
 
 def game_detail(request, id):
-    game = get_object_or_404(Game, id=id)
+    game = get_object_or_404(Game, id=id, user=request.user)
     return render(request, 'games/game_detail.html', {'game': game})
 
 def create_game(request):
     if request.method == 'POST':
         form = GameForm(request.POST)
         if form.is_valid():
-            form.save()
+            game = form.save(commit=False)
+            game.user = request.user
+            game.save()
             return redirect('games:game_list')
     else:
         form = GameForm()
@@ -24,7 +26,7 @@ def create_game(request):
     return render(request, 'games/create_game.html', {'form': form})
 
 def update_game(request, id):
-    game = get_object_or_404(Game, id=id)
+    game = get_object_or_404(Game, id=id, user=request.user)
 
     if request.method == 'POST':
         form =GameForm(request.POST, instance=game)
@@ -38,7 +40,7 @@ def update_game(request, id):
     return render(request, 'games/update_game.html', {'form': form})
 
 def delete_game(request, id):
-    game = get_object_or_404(Game, id=id)
+    game = get_object_or_404(Game, id=id, user=request.user)
 
     if request.method == 'POST':
         game.delete()
