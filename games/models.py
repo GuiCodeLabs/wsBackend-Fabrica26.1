f76824b272de4709a5322afa483b1cf930c41e3b
Guilherme <guilhermebeserra2007@gmail.com>
2026-03-20 23:04:46 -0300

    feat: make api_id optional in game model

--- games/models.py
+++ games/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.URLField(blank=True)
+    release_date = models.DateField(null=True, blank=True)
+    api_id = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title   
