7c928db470a01a2cef695c740b1592160228c372
Guilherme <guilhermebeserra2007@gmail.com>
2026-03-21 08:37:19 -0300

    feat: add create game form and view

--- games/urls.py
+++ games/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import game_list, game_detail
+from .views import game_list, game_detail, create_game
 
 app_name = 'games'
 
 urlpatterns = [
     path('', game_list, name='game_list'),
-    path('game/<int:id>/', game_detail, name='game_detail')
+    path('game/<int:id>/', game_detail, name='game_detail'),
+    path('create/', create_game, name='create_game'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Game
+from .forms import GameForm
+
 
 def game_list(request):
     games = Game.objects.all()
@@ -8,3 +10,15 @@ def game_list(request):
 def game_detail(request, id):
     game = get_object_or_404(Game, id=id)
     return render(request, 'games/game_detail.html', {'game': game})
+
+def create_game(request):
+    if request.method == 'POST':
+        form = GameForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('games:game_list')
+    else:
+        form = GameForm()
+
+    return render(request, 'games/create_game.html', {'form': form})
+
